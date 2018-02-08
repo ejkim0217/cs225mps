f3914ba80bd8555f21b65a0a0cc5846bd3e9fa1f
Thomas Driscoll <tfd2@linux-a3.ews.illinois.edu>
2018-02-07 20:22:32 -0600

    file submission

--- mp1/mp1.cpp
+++ mp1/mp1.cpp
@@ -30,7 +30,37 @@ void rotate(std::string inputFile, std::string outputFile) {
 PNG myArt(unsigned int width, unsigned int height) {
   PNG png(width, height);
   // Part 3
+  unsigned int mid_w = width/2;
 
+  for(unsigned int i=0; i<mid_w; i++)
+  {
+    for(unsigned int j=0; j<height; j++)
+    {
+      HSLAPixel *pixel = png.getPixel(i,j);
+      pixel->h = 310 * (400-i)/400;
+      /*This formula is meant to simulate the Doppler Effect. 400 can be considered
+      the speed of light in this artifical medium and i could be the velocity of
+      the source relative to the observer as it moves. I inverted the formula with
+      1/((v/v-vs)*f) so that the values obtained matched up with the [0, 360] degree
+      wheel I was limited with.
+      */
+      pixel->s = 1.0;
+      pixel->l = .5;
+      pixel->a = 1.0;
+    }
+  }
+  for(unsigned int i=mid_w; i<width; i++)
+  {
+    for(unsigned int j=0; j<height; j++)
+    {
+      HSLAPixel *pixel = png.getPixel(i,j);
+      pixel->h = -1*310*(400-i)/400;    //Same as above
+      pixel->s = 1.0;
+      pixel->l = .5;
+      pixel->a = 1.0;
+    }
+  }
+  /*
   for(unsigned int i=0; i<width; i++)
   {
     for(unsigned int j=0; j<height; j++)
@@ -78,7 +108,7 @@ PNG myArt(unsigned int width, unsigned int height) {
       pixel->a = 1.0;
     }
   }
-
+  */
 
   return png;
 }
